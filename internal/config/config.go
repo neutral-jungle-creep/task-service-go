da103773af29e0f0af3492c6a91c7fb419735932
Ковалева Екатерина <e.kovaleva@gaskar.group>
2025-08-23 20:53:17 +0300

    добавила пакет конфиг

---
+++ internal/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	defaultLogLevel = "info"
+)
+
+type Config struct {
+	ServiceName string           `json:"serviceName"`
+	ReleaseID   string           `json:"releaseId"`
+	LogLevel    string           `json:"logLevel"`
+	HTTPServer  HTTPServerConfig `json:"httpServer"`
+}
+
+type HTTPServerConfig struct {
+	ListenPort        string        `json:"apiListenPort"`
+	KeepAliveTime     time.Duration `json:"keepAliveTime"`
+	KeepAliveTimeout  time.Duration `json:"keepAliveTimeout"`
+	ReadHeaderTimeout time.Duration `json:"keepAliveReadHeaderTimeout"`
+	ReadTimeout       time.Duration `json:"readTimeout"`
+}
+
+func NewConfigFromEnv() (*Config, error) {
+	var configPath string
+
+	flag.StringVar(&configPath, "path", "config", "Path to config file")
+	flag.StringVar(&configPath, "p", "config", "Path to config file")
+	flag.Parse()
+
+	config := newDefaultConfig()
+
+	err := processEnv(config, configPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable process env")
+	}
+
+	return config, nil
+}
+
+func newDefaultConfig() *Config {
+	return &Config{
+		LogLevel: defaultLogLevel,
+	}
+}
+
+func processEnv(config *Config, configPath string) error {
+	file, err := os.Open(configPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, file)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(buf.Bytes(), config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
