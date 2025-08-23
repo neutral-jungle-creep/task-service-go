107b6a99276218a3c67f600400ae607a0968a865
Ковалева Екатерина <e.kovaleva@gaskar.group>
2025-08-23 19:38:43 +0300

    добавила пакет server

---
+++ pkg/http_server/server.go
@@ -0,0 +1,31 @@
+package http_server
+
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	defaultReadTimeout       = 10 * time.Second
+	defaultWriteTimeout      = 10 * time.Second
+	defaultIdleTimout        = 60 * time.Second
+	defaultReadHeaderTimeout = 10 * time.Second
+	defaultAddr              = ":8008"
+)
+
+func NewServer(handler http.Handler, opts ...Option) *http.Server {
+	s := &http.Server{
+		Addr:              defaultAddr,
+		Handler:           handler,
+		IdleTimeout:       defaultIdleTimout,
+		ReadTimeout:       defaultReadTimeout,
+		WriteTimeout:      defaultWriteTimeout,
+		ReadHeaderTimeout: defaultReadHeaderTimeout,
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	return s
+}
